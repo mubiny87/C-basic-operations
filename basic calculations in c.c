7ab62e17271a0e15560581c3473a441723257491
mubiny87 <92442164+mubiny87@users.noreply.github.com>
2025-02-24 09:24:26 +0600

    +-*/% basic calculator
    My first upload to github. Learning C and github terminologies at the same time, seems fun not gonna lie. Writing this from my Uni's general lab. 2/24/2025 9:24 am Monday

---
+++ basic calculations in c.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int a,b;
+printf("Enter 1st number\n");
+scanf("%d", &a);
+printf("Enter 2nd number\n");
+scanf("%d", &b);
+printf("Here are the basic calculations given as follows \n summation %d\n difference %d\n multiplication %d\n division %f\n modulas %d\n", a+b, a-b, a*b, (float)a/b, a%b);
+return 0;
+}
