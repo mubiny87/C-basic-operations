4e74011aa6a14c9a144c35a11e18f0d33f7094f9
mubiny87 <92442164+mubiny87@users.noreply.github.com>
2025-02-24 09:29:58 +0600

    pause system to wait for user to see results
    learned the pause system to stop vanishing out before user can see the calculations

--- basic calculations in c.c
+++ basic calculations in c.c
@@ -8,5 +8,6 @@ scanf("%d", &a);
 printf("Enter 2nd number\n");
 scanf("%d", &b);
 printf("Here are the basic calculations given as follows \n summation %d\n difference %d\n multiplication %d\n division %f\n modulas %d\n", a+b, a-b, a*b, (float)a/b, a%b);
+    system("pause");
 return 0;
 }
